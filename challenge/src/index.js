2fc1ae321ee6d3425cc3d4d39bcb085333272a66
Sasha Vodnik <sv@sashavodnik.com>
2019-12-20 12:08:03 -0800

    set up files for challenge video

---
+++ challenge/src/index.js
@@ -0,0 +1,11 @@
+let heading = 'Integrating ESLint--Challenge';
+
+let double = (num) => {
+  let result = num * 2;
+  return result;
+  console.log(result);
+};
+
+let days=double( 365 );
+let heading=`Two years contain ${ days } days`;
+console.log(heading);
