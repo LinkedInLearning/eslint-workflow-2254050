e59e815e6327a8b7216a86f71b6e5f4080e87875
Sasha Vodnik <sv@sashavodnik.com>
2019-12-19 16:52:53 -0800

    finished recording 01_04

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 const text = 'Building with ESLint';
-console.log(text);
+console.log(text); // eslint-disable-line no-console
+
+for (let i = 10; i <= 0; i += 1) {
+  // eslint-disable-next-line no-console
+  console.log(i);
+}
